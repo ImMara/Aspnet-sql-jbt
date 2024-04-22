c126416c8d6a173be69340e663523c367aa440ae
ImMara <alaneraerts@live.be>
2024-04-22 16:42:38 +0200

    sql request in controller - X view bugged

--- API/Controllers/HomeController.cs
+++ API/Controllers/HomeController.cs
@@ -1,16 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using MySqlConnector;
 
 namespace API.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var ids = new List<int>();
+            try
+            {
+                // TOFIX: Connection string is hardcoded
+                var connection = new MySqlConnection("Server=localhost,8889;UserID=root;Password=root;Database=aspnet");
+                await connection.OpenAsync();
+                
+                var command = new MySqlCommand("SELECT * FROM Applications;", connection);
+                var reader = await command.ExecuteReaderAsync();
+                
+                while (await reader.ReadAsync())
+                {
+                    ids.Add(reader.GetInt32(0));
+                }
+                Console.WriteLine(ids.Count);
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            // TOFIX: Index view is not found
+            return View("Index", ids);
         }
 
         public ActionResult About()
