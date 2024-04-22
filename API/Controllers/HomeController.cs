9227156fc49f807df4b67e29c2b0e48a827ed94a
ImMara <alaneraerts@live.be>
2024-04-23 00:34:56 +0200

    fix view error + sql management

--- API/Controllers/HomeController.cs
+++ API/Controllers/HomeController.cs
@@ -1,39 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
+using API.Models;
 using MySqlConnector;
 
 namespace API.Controllers
 {
     public class HomeController : Controller
     {
-        public async Task<ActionResult> Index()
+        public ActionResult Index(  )
         {
-            var ids = new List<int>();
+            var model = new IndexViewModel
+            {
+                Ids = new List<int>()
+            };
+            
             try
             {
-                // TOFIX: Connection string is hardcoded
                 var connection = new MySqlConnection("Server=localhost,8889;UserID=root;Password=root;Database=aspnet");
-                await connection.OpenAsync();
-                
+                connection.Open();
+
                 var command = new MySqlCommand("SELECT * FROM Applications;", connection);
-                var reader = await command.ExecuteReaderAsync();
-                
-                while (await reader.ReadAsync())
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
                 {
-                    ids.Add(reader.GetInt32(0));
+                    model.Ids.Add(reader.GetInt32(0));
                 }
-                Console.WriteLine(ids.Count);
-            }catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
+
+                Console.WriteLine(model.Ids.Count);
             }
-            
-            // TOFIX: Index view is not found
-            return View("Index", ids);
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.ToString());
+                return View("Error", new HandleErrorInfo(e, "Home", "Index"));
+
+            }
+            return View("Index", model);
         }
 
         public ActionResult About()
---
+++ API/Models/IndexViewModel.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace API.Models
+{
+    public class IndexViewModel
+    {
+        public List<int> Ids { get; set; }
+    }
+}
